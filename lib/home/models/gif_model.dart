f09e586d360d4805a964e7512cd3a84eb0485fec
Leonardo Silveira Berlatto <leonardo.berlatto@cwi.com.br>
2020-02-05 15:00:51 -0300

    🎨 Removing slug from gif model

--- lib/home/models/gif_model.dart
+++ lib/home/models/gif_model.dart
@@ -3,7 +3,6 @@ import 'package:gif_searcher/home/models/image_model.dart';
 class GifModel {
   String id;
   String url;
-  String slug;
   String bitlyGifUrl;
   String bitlyUrl;
   String title;
@@ -12,14 +11,12 @@ class GifModel {
   GifModel(
       {String id,
       String url,
-      String slug,
       String bitlyGifUrl,
       String bitlyUrl,
       String title,
       List<ImageModel> images}) {
     this.id = id;
     this.url = url;
-    this.slug = slug;
     this.bitlyGifUrl = bitlyGifUrl;
     this.bitlyUrl = bitlyUrl;
     this.title = title;
@@ -29,7 +26,6 @@ class GifModel {
   GifModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     url = json['url'];
-    slug = json['slug'];
     bitlyGifUrl = json['bitly_gif_url'];
     bitlyUrl = json['bitly_url'];
     title = json['title'];
