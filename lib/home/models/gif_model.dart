f45aca11b70d45e902b049924ebb9ac8a0ad30fd
leonardo-berlatto <leoberlatto@gmail.com>
2020-02-16 15:54:32 -0300

    ♻️ Cleaning up GifModel code

--- lib/home/models/gif_model.dart
+++ lib/home/models/gif_model.dart
@@ -3,31 +3,34 @@ import 'package:gif_searcher/home/models/image_model.dart';
 class GifModel {
   String id;
   String url;
+  String source;
   String bitlyGifUrl;
   String bitlyUrl;
   String title;
-  List<ImageModel> images;
+  Map<String, ImageModel> images;
 
   GifModel(
-      {String id,
-      String url,
-      String bitlyGifUrl,
-      String bitlyUrl,
-      String title,
-      List<ImageModel> images}) {
-    this.id = id;
-    this.url = url;
-    this.bitlyGifUrl = bitlyGifUrl;
-    this.bitlyUrl = bitlyUrl;
-    this.title = title;
-    this.images = images;
-  }
+      {this.id,
+      this.url,
+      this.source,
+      this.bitlyGifUrl,
+      this.bitlyUrl,
+      this.title,
+      this.images});
 
   GifModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     url = json['url'];
+    source = json['source'];
     bitlyGifUrl = json['bitly_gif_url'];
     bitlyUrl = json['bitly_url'];
     title = json['title'];
+    images = _getImagesFromMap(json['images']);
+  }
+
+  _getImagesFromMap(Map<String, dynamic> images) {
+    return images
+        .map((key, value) => MapEntry(key, ImageModel.fromJson(value)))
+        .cast<String, ImageModel>();
   }
 }
